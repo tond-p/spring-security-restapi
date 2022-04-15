c916ed72e3bbe65c5ec49a5693d4fc8e8a58db7e
seonji0326 <seonji0326@gmail.com>
2022-04-15 17:39:53 +0900

    logout추가

--- src/main/java/com/example/restapi/sample/SampleController.java
+++ src/main/java/com/example/restapi/sample/SampleController.java
@@ -1,5 +1,12 @@
 package com.example.restapi.sample;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +23,23 @@ public class SampleController {
   @GetMapping(value = "/login-test")
   public String loginTest() {
     return "login_user";
+  }
+
+  @PostMapping(value = "/my-logout")
+  public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    HttpSession session = request.getSession();
+    Cookie cookie = new Cookie("remember-me", null);
+
+    if (auth != null) {
+      cookie.setMaxAge(0);
+      response.addCookie(cookie); // remember-me 쿠키삭제
+
+      session.invalidate();
+      new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+
+    return "logout success!";
   }
 }
